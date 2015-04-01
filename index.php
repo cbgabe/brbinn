4a4c78b23f8237e4fbbdd938058ba908b74f9539
Gabriel Carpenedo <cbgabe@gmail.com>
2015-04-01 01:45:28 -0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,53 @@
+<html>
+  <head>
+    <meta charset="utf-8"></meta>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=2.0, user-scalable=yes"></meta>
+    <link href='http://fonts.googleapis.com/css?family=Lato:400,700,900' rel='stylesheet' type='text/css'></link>
+    <link href='bootstrap.css' rel='stylesheet' type='text/css'></link>
+    <script type='text/javascript' src="config.json" ></script>
+    <title>brbinn - Reservas de quartos de hóteis durante o dia ou tarde</title>
+  </head>
+
+  <body style="font-family: 'Lato', sans-serfi; font-weight: 400; text-align: center; margin: 50px 0 60px 0; color: #333">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+          <div style="font-size: 28px; font-weight: 900; color: #3498db;">brbinn</div>
+          <h1 style="font-size: 48px; font-weight: 900; padding-bottom: 0px; margin-bottom: 5px; line-height: 1em;">Reserve quarto de hotéis em horários alternativos</h1>
+          <h3 style="font-size: 24px; font-weight: 400; margin-top: 10px; line-height: 1.2em; color: #666">Quer fazer uma parada rápida? Hospede-se um hotel durante o horário comercial e pague menos.</h3>
+          <a href="mailto:brbinnreservas@gmail.com?subject=Olá, sou dono de um hotel!">É dono de hotel? Entre em contato!</a>
+        </div>
+      </div>
+      <br>
+
+      <div class="row">
+        <div class="col-sm-12 col-md-6 col-sm-offset-1 col-md-offset-3" style="background-color: #f2f2f2; padding: 30px 40px; border-radius: 10px; margin-top: 30px">
+          <div id="wufoo-m18dzceg0mhyo1x">
+            Fill out my <a href="https://brbinn.wufoo.com/forms/m18dzceg0mhyo1x">online form</a>.
+          </div>
+          <div id="wuf-adv" style="font-family:inherit;font-size: small;color:#a7a7a7;text-align:center;display:block;">Use <a href="http://www.wufoo.com/gallery/templates/">Wufoo templates</a> to make your own HTML forms.</div>
+
+          <script type="text/javascript">var m18dzceg0mhyo1x;(function(d, t) {
+          var s = d.createElement(t), options = {
+          'userName':'brbinn',
+          'formHash':'m18dzceg0mhyo1x',
+          'autoResize':true,
+          'height':'727',
+          'async':true,
+          'host':'wufoo.com',
+          'header':'show',
+          'ssl':true};
+          s.src = ('https:' == d.location.protocol ? 'https://' : 'http://') + 'www.wufoo.com/scripts/embed/form.js';
+          s.onload = s.onreadystatechange = function() {
+          var rs = this.readyState; if (rs) if (rs != 'complete') if (rs != 'loaded') return;
+          try { m18dzceg0mhyo1x = new WufooForm();m18dzceg0mhyo1x.initialize(options);m18dzceg0mhyo1x.display(); } catch (e) {}};
+          var scr = d.getElementsByTagName(t)[0], par = scr.parentNode; par.insertBefore(s, scr);
+          })(document, 'script');</script>
+        </div>
+      </div>
+
+    </div>
+
+  </body>
+
+</html>
